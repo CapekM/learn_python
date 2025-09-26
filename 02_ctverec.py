f810019bdf1b469b8f614389dd8b4013dbf193da
capek <martincapku@seznam.cz>
2025-09-26 09:15:20 +0200

    first

---
+++ 02_ctverec.py
@@ -0,0 +1,5 @@
+# Tento program počítá obvod a obsah čtverce.
+
+x = float(input('Zadej stranu čtverce v centimetrech: '))
+print("Obvod čtverce se stranou", x, "cm je", x * 4, "cm")
+print("Obsah čtverce se stranou", x, "cm je", x * x, "cm2")
---
+++ 03_operators.py
@@ -0,0 +1,7 @@
+pravda = 1 < 3
+print(type(pravda))
+print(pravda)
+print(True)
+
+nepravda = 1 == 3
+print(nepravda)
